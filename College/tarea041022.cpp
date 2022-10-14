06a7a346c940207a461ea2c993c92bd616a4d544
Diego Yegros <diegoyegrosa@gmail.com>
2022-10-14 10:23:58 -0300

    configuring vscode

--- College/tarea041022.cpp
+++ College/tarea041022.cpp
@@ -1,3 +1,8 @@
 /*
-Esta es una tarea que tengo que quitar del aula virtual
+Crear un sistema en C++ que administre una financiera de nombre FINANZAS SA. El mismo debe permitir la carga de un usuario con los siguientes datos: nombre, apellido, cedula de identidad, el número de cuenta. Además debe permitir el depósito de dinero en la cuenta del mismo e ir acumulándolo, también debe permitir la extracción del dinero de la cuenta mientras que no supere el monto total actual.
 */
+#include <iostream>
+using namespace std;
+int main()
+{
+}
