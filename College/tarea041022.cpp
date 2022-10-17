b7334f78f9b480e57ef681180f123fb0ab068046
Diego Yegros <diegoyegrosa@gmail.com>
2022-10-17 08:14:26 -0300

    DoYourFuckingHomework

--- College/tarea041022.cpp
+++ College/tarea041022.cpp
@@ -1,8 +1,27 @@
 /*
-Crear un sistema en C++ que administre una financiera de nombre FINANZAS SA. El mismo debe permitir la carga de un usuario con los siguientes datos: nombre, apellido, cedula de identidad, el número de cuenta. Además debe permitir el depósito de dinero en la cuenta del mismo e ir acumulándolo, también debe permitir la extracción del dinero de la cuenta mientras que no supere el monto total actual.
+Crear un sistema en C++ que administre una financiera de nombre FINANZAS SA.
+El mismo debe permitir la carga de un usuario con los siguientes datos: nombre, apellido, cedula de identidad, el número de cuenta. Además debe permitir el depósito de dinero en la cuenta del mismo e ir acumulándolo, también debe permitir la extracción del dinero de la cuenta mientras que no supere el monto total actual.
 */
 #include <iostream>
 using namespace std;
+
+class financiera
+{
+    // atributos
+    char apellido[15];
+    char nombre[15];
+    int nrocuenta;
+    int ci;
+    // metodos
+    void usuario();
+    void deposito();
+    void extraccion();
+};
 int main()
 {
+    int opc = 0;
+    while (opc != 9)
+    {
+    }
+    return 0;
 }
