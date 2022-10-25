6c5c39ce93ff85e5ffa36653c3719684fa4b22a6
Diego Yegros <64743423+DiegoYegros@users.noreply.github.com>
2022-10-25 16:51:30 -0500

    statement

---
+++ College/clase251022.cpp
@@ -0,0 +1,6 @@
+// Ejercicio de pogramacion utilizando Herencia Multiple.
+// Una floreria de nombre floripondio se dedica a la venta de regalos, el regalo consta de lo siguiente: una flor y una tarjeta.
+// De la flor se necesita los sisguientes datos. el nombre, la especie y el color..
+// La tarjeta debe tener un mensaje.
+// Ademas del regalo se debe saber el precio, la direccion de envio, la fecha de envio, el emisor y el destinatario.
+// Crear un sisteam en C++ utilizando herencia multiple, que permita administrar lo solicitado.
