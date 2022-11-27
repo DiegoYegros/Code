91d0705bbb2eb87a24c1a74c9ac329cbf0adc822
Diego Yegros <diegoyegrosa@gmail.com>
2022-11-27 18:11:17 -0300

    Java

--- College/Estructura de Datos/clase251022.cpp
+++ College/Estructura de Datos/clase251022.cpp
@@ -1,57 +1,66 @@
-//Ejercicio1
-// Ejercicio de pogramacion utilizando Herencia Multiple.
-// Una floreria de nombre floripondio se dedica a la venta de regalos, el regalo consta de lo siguiente: una flor y una tarjeta.
-// De la flor se necesita los sisguientes datos. el nombre, la especie y el color..
-// La tarjeta debe tener un mensaje.
-// Ademas del regalo se debe saber el precio, la direccion de envio, la fecha de envio, el emisor y el destinatario.
-// Crear un sisteam en C++ utilizando herencia multiple, que permita administrar lo solicitado.
+// Ejercicio1
+//  Ejercicio de pogramacion utilizando Herencia Multiple.
+//  Una floreria de nombre floripondio se dedica a la venta de regalos, el regalo consta de lo siguiente: una flor y una tarjeta.
+//  De la flor se necesita los sisguientes datos. el nombre, la especie y el color..
+//  La tarjeta debe tener un mensaje.
+//  Ademas del regalo se debe saber el precio, la direccion de envio, la fecha de envio, el emisor y el destinatario.
+//  Crear un sisteam en C++ utilizando herencia multiple, que permita administrar lo solicitado.
 #include <iostream>
 #include <string>
 using namespace std;
-class flor{
-    public: //atributos
+class flor
+{
+public: // atributos
     string nombre;
     string color;
     string especie;
-    public: //metodos
+
+public: // metodos
     void cargarFlor();
     void mostrarFlor();
     flor();
 };
 
-class tarjeta{
-    public: //atributos
+class tarjeta
+{
+public: // atributos
     string mensaje;
-    public: //metodos
+
+public: // metodos
     void cargarmensaje();
     void mostrarmensaje();
     tarjeta();
 };
 
-class regalo : public flor, public tarjeta{
-    public: //atributos
+class regalo : public flor, public tarjeta
+{
+public: // atributos
     int precio;
     string fecha;
     string direccion;
     string emisor;
     string destinatario;
-    public: //metodos
+
+public: // metodos
     void cargarRegalo();
     void mostrarRegalo();
     int buscarFlor();
 }
 
-
-void flor::flor(){
+void
+flor::flor()
+{
     nombre = "";
     color = "";
     especie = "";
 }
-tarjeta::tarjeta(){
-    mensaje ="";
+tarjeta::tarjeta()
+{
+    mensaje = "";
 }
 
-regalo::regalo(){
+regalo::regalo()
+{
     precio = 0;
     direccion = "";
     fechaenvio = "";
@@ -60,47 +69,71 @@ regalo::regalo(){
 }
 void menu();
 
-void flor()::cargarFlor{
-    cout<<system("cls");
-    cout<<"Nombre de la flor: "; cin>>nombre;
-    cout<<"Color de la flor: "; cin>>color;
-    cout<<"Especie de la flor: "; cin>>especie;
+void flor()::cargarFlor
+{
+    cout << system("cls");
+    cout << "Nombre de la flor: ";
+    cin >> nombre;
+    cout << "Color de la flor: ";
+    cin >> color;
+    cout << "Especie de la flor: ";
+    cin >> especie;
 }
-void flor()::mostrarFlor{
-    cout<<system("cls");
-    cout<<"Nombre de la flor: "; nombre;
-    cout<<"Color de la flor: "; color;
-    cout<<"Especie de la flor: "; especie;
-}
-
-void regalo()::cargarRegalo{
-    system("cls");
-    cout<<"Cargar destinatario: "; cin>>destinatario;
-    cout<<"Cargar emisor: "; cin>>emisor;
-    cout<<"Cargar direccion: "; cin>>direccion;
-    cout<<"Cargar fecha: "; cin>>fecha;
-}
-void regalo()::mostrarRegalo{
-    system("cls");
-    cout<<"Cargar destinatario: "; destinatario;
-    cout<<"Cargar emisor: "; emisor;
-    cout<<"Direccion: "; direccion;
-    cout<<"Fecha: "; fecha;
+void flor()::mostrarFlor
+{
+    cout << system("cls");
+    cout << "Nombre de la flor: ";
+    nombre;
+    cout << "Color de la flor: ";
+    color;
+    cout << "Especie de la flor: ";
+    especie;
 }
 
-void tarjeta()::cargarmensaje{
+void regalo()::cargarRegalo
+{
     system("cls");
-    cout<<"Ingrese su mensaje: "; cin>>mensaje;
+    cout << "Cargar destinatario: ";
+    cin >> destinatario;
+    cout << "Cargar emisor: ";
+    cin >> emisor;
+    cout << "Cargar direccion: ";
+    cin >> direccion;
+    cout << "Cargar fecha: ";
+    cin >> fecha;
 }
-void tarjeta()::mostrarmensaje{
+void regalo()::mostrarRegalo
+{
     system("cls");
-    cout<<"Mensaje: "; mensaje;
+    cout << "Cargar destinatario: ";
+    destinatario;
+    cout << "Cargar emisor: ";
+    emisor;
+    cout << "Direccion: ";
+    direccion;
+    cout << "Fecha: ";
+    fecha;
 }
-int regalo()::buscarFlor{
+
+void tarjeta()::cargarmensaje
+{
+    system("cls");
+    cout << "Ingrese su mensaje: ";
+    cin >> mensaje;
+}
+void tarjeta()::mostrarmensaje
+{
+    system("cls");
+    cout << "Mensaje: ";
+    mensaje;
+}
+int regalo()::buscarFlor
+{
     string buscar;
     int flag = 0;
-    cout<<"Buscar flor: "; cin>>buscar;
-    if(buscar == nombre)
+    cout << "Buscar flor: ";
+    cin >> buscar;
+    if (buscar == nombre)
     {
         bandera = 1;
         return bandera;
@@ -108,50 +141,57 @@ int regalo()::buscarFlor{
     return bandera;
 }
 
-int main(){
+int main()
+{
     menu();
     return 0;
 }
 
-void menu(){
+void menu()
+{
     regalo cliente;
     int opc = 0;
-    while (opc != 4){
-    system("cls");
-    cout<<"\t\tFloreria Honor Colorado\n";
-    cout<<"1. Cargar regalo\n";
-    cout<<"2. Mostrar regalo\n";
-    cout<<"3. Buscar la flor y ver los datos\n";
-    cout<<"4. Salir\n";
-    cout<<"Ingrese la opcion que quiera: "; cin>>opc;
-    switch(opc){
+    while (opc != 4)
+    {
+        system("cls");
+        cout << "\t\tFloreria Honor Colorado\n";
+        cout << "1. Cargar regalo\n";
+        cout << "2. Mostrar regalo\n";
+        cout << "3. Buscar la flor y ver los datos\n";
+        cout << "4. Salir\n";
+        cout << "Ingrese la opcion que quiera: ";
+        cin >> opc;
+        switch (opc)
+        {
         case 1:
             cliente.cargarFlor();
             cliente.cargarmensaje();
             cliente.cargarRegalo();
-        break;
+            break;
         case 2:
             mostrarRegalo();
-        break;
+            break;
         case 3:
-            if(cliente.buscarflor()==1){
-            mostrarFlor();
+            if (cliente.buscarflor() == 1)
+            {
+                mostrarFlor();
             }
-            else{
+            else
+            {
                 system("cls");
-                cout<<"No se ha encontrado una flor. Presione enter para volver al menu.";
+                cout << "No se ha encontrado una flor. Presione enter para volver al menu.";
                 system("pause");
             }
-        break;
+            break;
         case 4:
-        system("cls");
-        cout("Gracias por usar este programa. Hasta la proxima.");
-        system("pause");
-        break;
-        default: 
-        system("cls");
-        cout("Opcion invalida. Vuelva a intentarlo.");
-        system("pause");
+            system("cls");
+            cout("Gracias por usar este programa. Hasta la proxima.");
+            system("pause");
+            break;
+        default:
+            system("cls");
+            cout("Opcion invalida. Vuelva a intentarlo.");
+            system("pause");
+        }
     }
-}
 }
