2c535573b2221db98fd9016a2d973696479275ce
Diego Yegros <diegoyegrosa@gmail.com>
2022-11-28 12:15:39 -0300

    nextInt, nextLine clarification

--- Java/main.java
+++ Java/main.java
@@ -4,9 +4,7 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your age?");
-        int age = scanner.nextInt();
-        // Cleans up the buffer
-        scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
         System.out.println("What is your name?");
         String name = scanner.nextLine();
         String formattedString = String.format("Hi, my name is %s and I am %d", name, age);
