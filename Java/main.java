d84b621c6626a96e285f218c1355fa56ee3fc4f3
Diego Yegros <diegoyegrosa@gmail.com>
2022-11-28 11:47:48 -0300

    Cleaning up the buffer

--- Java/main.java
+++ Java/main.java
@@ -4,10 +4,12 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your age?");
-        String age = scanner.nextLine();
+        int age = scanner.nextInt();
+        // Cleans up the buffer
+        scanner.nextLine();
         System.out.println("What is your name?");
         String name = scanner.nextLine();
-        String formattedString = String.format("Hi, my name is %s and I am %s", name, age);
+        String formattedString = String.format("Hi, my name is %s and I am %d", name, age);
         System.out.println(formattedString);
         scanner.close();
 
